546b3d48c137dfdabcef0f12278af5ea2988b3d7
michallibov <92799624+michallibov@users.noreply.github.com>
2022-04-08 19:42:40 +0300

    Update combined_tools.py

--- combined_tools.py
+++ combined_tools.py
@@ -6,8 +6,7 @@ def interleave(*iterables):
     chains all the objects by their indexes. This function first chains all the
     first indexes of all the iterables, then the it chains by the second and so on.
     :param iterables: iterable objects that we will chain to one list
-    :return: a chained by indexes list of the iterables we got. The generator returns
-    one cell of the combined list each time.
+    :return: a chained by indexes list of the iterables we got using yield.
     """
     yield [character for sub_iterable in zip(*iterables) for character in sub_iterable]
 
