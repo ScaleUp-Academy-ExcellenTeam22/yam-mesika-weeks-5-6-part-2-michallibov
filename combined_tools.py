3e17f337b9bc03367f7133472c185c8c88036ff6
michallibov <92799624+michallibov@users.noreply.github.com>
2022-04-08 18:15:19 +0300

    Changed the function to a generator.

--- combined_tools.py
+++ combined_tools.py
@@ -1,14 +1,17 @@
 
+
 def interleave(*iterables):
     """
-    This function creates a list from all the iterable objects we received that
+    This generator creates a list from all the iterable objects we received that
     chains all the objects by their indexes. This function first chains all the
     first indexes of all the iterables, then the it chains by the second and so on.
     :param iterables: iterable objects that we will chain to one list
-    :return: a chained by indexes list of the iterables we got
+    :return: a chained by indexes list of the iterables we got. The generator returns
+    one cell of the combined list each time.
     """
-    return [item for sub_iterable in zip(*iterables) for item in sub_iterable]
+    yield [character for sub_iterable in zip(*iterables) for character in sub_iterable]
 
 
 if __name__ == '__main__':
-    print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    for item in interleave('abc', [1, 2, 3], ('!', '@', '#')):
+        print(item)
