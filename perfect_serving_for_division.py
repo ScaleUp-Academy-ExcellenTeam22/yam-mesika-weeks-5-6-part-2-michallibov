ecf6c5774b0fb3187b0f931ecd3a561d23de376a
michallibov <92799624+michallibov@users.noreply.github.com>
2022-04-09 21:32:50 +0300

    Changed to list comprehension.

--- perfect_serving_for_division.py
+++ perfect_serving_for_division.py
@@ -1,4 +1,5 @@
 
+
 def find_perfect_serving():
     """
     This is a generator that runs in an endless loop and returns using yield if the sum of all it's
@@ -8,14 +9,13 @@ def find_perfect_serving():
     """
     # The number we want to check if it's division's sum equals to it.
     number_of_serving = 1
+    # The sum of all divisions
+    sum_of_divisions = 0
     while True:
-        # The sum of all divisions
-        sum_of_divisions = 0
-        for serving_division in range(1, number_of_serving-1):
-            # Check if this number is a division of number_of_serving and if so, add it to the sum_of_divisions
-            sum_of_divisions += serving_division if number_of_serving % serving_division == 0 else 0
-        if sum_of_divisions == number_of_serving:
-            yield number_of_serving
+        # The result variable is just to prevent a warning that the statement has no effect
+        result = [(yield number_of_serving) if sum(sum_of_divisions + serving_division if number_of_serving %
+                  serving_division == 0 else 0 for serving_division in range(1, number_of_serving-1)) == number_of_serving else 0]
+
         number_of_serving += 1
 
 
